01f69bc06b73a21763e6e9eec450c0cba51acbc9
IvKosar <kosarevych@ucu.edu.ua>
2018-04-11 16:01:46 +0300

    removals

--- cmake-build-debug/CMakeFiles/cur.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cur.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/cur.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cur.
-# This may be replaced when dependencies are built.
